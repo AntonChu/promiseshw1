e3498c0c224c8ee96d69cb893504b100d18838f7
Anton <antonch92@gmail.com>
2023-01-30 01:04:35 +0100

    problem with promises chain

--- src/js/gameSaver.js
+++ src/js/gameSaver.js
@@ -1,7 +1,12 @@
+import json from "./parser";
+import read from "./reader";
+
 export default class GameSavingLoader {
   static load() {
-    const data = read(); // возвращается Promise!
-    const value = json(data); // возвращается Promise!
-    return value;
+    const data = read();
+    .then (() => {
+      const value = json(data);
+      return value;
+    })
   }
 }
