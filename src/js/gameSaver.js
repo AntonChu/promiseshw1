1972105a27d041d0b2760adbd6f458ceadd4427d
Anton <antonch92@gmail.com>
2023-02-04 17:14:11 +0100

    test does not work

--- src/js/gameSaver.js
+++ src/js/gameSaver.js
@@ -1,65 +1,12 @@
-// import { resolve } from "core-js/fn/promise";
-// import json from "./parser";
-// import read from "./reader";
+import json from './parser';
+import read from './reader';
 
-
-function json(data) {
-  return new Promise((resolve, reject) => {
-    // эмуляция обработки ArrayBuffer
-    setTimeout(() => {
-      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-    }, 500);
-  });
-}
-
-function read() {
-  return new Promise((resolve, reject) => {
-  // эмуляция чтения файла
-    setTimeout(() => {
-      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
-      return (input => {
-        const buffer = new ArrayBuffer(input.length * 2);
-        const bufferView = new Uint16Array(buffer);
-        for (let i = 0; i < input.length; i++) {
-          bufferView[i] = input.charCodeAt(i);
-        }
-        resolve(buffer);
-      })(data);
-    }, 1000); 
-  });
-}
-
-// class GameSavingLoader {
-//   static load() {
-//     read().then((result) => {
-//       console.log(result)
-//       json(result)
-//     }).then((newRes) => {
-//       console.log(newRes)
-//     })
-    
-//   }
-// }
-
-// class GameSavingLoader {
-//   static load() {
-//     read().then((res)=>{
-//       console.log(res);
-//     })
-//   }
-// }
-
-class GameSavingLoader {
-  async load() {
-    const res = await res();
-    read().then((res)=>{
-      write().then((nextRes)=>{
-        return nextRes;
-      })
-    })
+export default class GameSavingLoader {
+  static load() {
+    return read().then(result => { // eslint-disable-line
+      return json(result);
+    }).then(result => { // eslint-disable-line
+      return result;
+    });
   }
 }
-
-
-const anton = GameSavingLoader;
-anton.load();
